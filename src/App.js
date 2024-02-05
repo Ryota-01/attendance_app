e399b85dfdb323874cc67fef6275aff497a5bcd0
Ryota Nishida <24.ryota.sub@gmail.com>
2024-02-06 00:03:01 +0900

    勤怠入力画面の作成

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './common/Header';
+import AttendanceInput from './common/AttendanceInput';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <AttendanceInput />
     </div>
   );
 }
