f31d28c3f16500e6fe764e332da15fe53559cf1f
Ryota Nishida <24.ryota.sub@gmail.com>
2024-02-09 10:39:11 +0900

    打刻画面の修正

--- src/components/AttendanceButton.js
+++ src/components/AttendanceButton.js
@@ -6,11 +6,25 @@ export default function AttendanceButton() {
   const [ clockInDisabled, setClockInDisabled ] = useState(false);
   const [ clockOutDisabled, setClockOutDisabled ] = useState(true);
   const [ currentDate, setCurrentDate ] = useState(getFormattedDate());
+  const [ attendanceList, setAttendanceList ] = useState([]);
 
+  //ローカルストレージから打刻時間のデータを取得
+  const getDateFromLocalStorage = () => {
+    const data = [];
+    for (let i = 0; i < localStorage.length; i++) {
+      const key = localStorage.key(i);
+      const value = JSON.parse(localStorage.getItem(key));
+      data.push({ date : key, ...value });
+    }
+    return data;
+  }
 
   useEffect(() => {
+    const dateFromLocalStorage = getDateFromLocalStorage();
     const storedClockInTime = localStorage.getItem(`${currentDate}-clockIn`);
+    const parsedClockInTime = storedClockInTime ? JSON.parse(storedClockInTime):null;
     const storedClockOutTime = localStorage.getItem(`${currentDate}-clockOut`);
+    setAttendanceList(dateFromLocalStorage);
     setClockInDisabled(!!storedClockInTime);
     setClockOutDisabled(!!storedClockOutTime);
   }, [currentDate]);
@@ -33,7 +47,7 @@ export default function AttendanceButton() {
   //出勤ボタンが押されたときの処理
   const handleClockIn = () => {
     const currentTime = new Date().toLocaleTimeString();
-    localStorage.setItem(`${currentDate}-clockIn`, currentTime);
+    localStorage.setItem(`${currentDate}-clockIn`, JSON.stringify({clockIn : currentTime}));
     alert('出勤しました');
     setClockInDisabled(true); //出勤ボタンをdisabledに
     setClockOutDisabled(false); //出勤ボタンをdisabledに
@@ -42,7 +56,7 @@ export default function AttendanceButton() {
   //退勤ボタンが押されたときの処理
   const handleClockOut = () => {
     const currentTime = new Date().toLocaleTimeString();
-    localStorage.setItem(`${currentDate}-clockOut`, currentTime);
+    localStorage.setItem(`${currentDate}-clockOut`, JSON.stringify({clockOut : currentTime}));
     alert('退勤しました。お疲れさまでした！');
     setClockOutDisabled(true); //退勤ボタンをdisabledに
   }
@@ -57,10 +71,32 @@ export default function AttendanceButton() {
     return `${year}-${month}-${day}`;
   }
 
+
+  const AttendanceList = () => {
+    return (
+      <div>
+        <h2>勤怠一覧</h2>
+        <ul>
+          {attendanceList.map((data) => (
+            <li key={data.date}>
+              {data.date} - 出勤打刻：{data.clockIn}　／　退勤打刻：{data.clockOut}
+            </li>
+          ))}
+        </ul>
+      </div>
+    )
+  }
+
   return (
-    <div className='attendanceButton'>
-      <button className='inButton' onClick={handleClockIn} disabled={clockInDisabled}>出勤</button>
-      <button className='outButton' onClick={handleClockOut} disabled={clockOutDisabled}>退勤</button>
+    <div>
+      <div className='attendanceButton'>
+        <button className='inButton' onClick={handleClockIn} disabled={clockInDisabled}>出勤</button>
+        <button className='outButton' onClick={handleClockOut} disabled={clockOutDisabled}>退勤</button>
+      </div>
+      <div>
+        <AttendanceList />
+      </div>
     </div>
+
   )
 }
