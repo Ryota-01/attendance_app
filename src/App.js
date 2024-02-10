4698ce5793fb2bb533c88b32ccadc825b70119db
Ryota Nishida <24.ryota.sub@gmail.com>
2024-02-10 14:16:57 +0900

    勤怠一覧画面の作成

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import './css/App.css';
 import './css/Common.css';
 import Header from './components/Header';
@@ -6,14 +7,21 @@ import AttendanceInput from './components/AttendanceInput';
 import AttendTotal from './components/AttendTotal';
 import DigitalClock from './components/DigitalClock';
 import AttendanceButton from './components/AttendanceButton';
+import AttendanceList from './components/AttendanceList';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <DigitalClock />
-      <AttendanceButton />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <DigitalClock />
+        <AttendanceButton />
+        <AttendanceList />
+        <Routes>
+          <Route path='/attendancelist' element={<AttendanceList />}></Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
