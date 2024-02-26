404fb9ffc9d3345363af37ed459277bc75d4f8ec
Ryota Nishida <24.ryota.sub@gmail.com>
2024-02-26 09:54:15 +0900

    勤怠一覧のfirestoreのCRUD操作修正

--- src/components/AttendanceButton.js
+++ src/components/AttendanceButton.js
@@ -62,7 +62,7 @@ export default function AttendanceButton() {
       }
     };
 
-    if(user) {
+    if (user) {
       fetchUserName();
     }
   }, [user]);
@@ -70,15 +70,26 @@ export default function AttendanceButton() {
   //出勤ボタンを押した時の処理
   const handleClockIn = async (e) => {
     e.preventDefault();
-    const userDocumentRef = doc(db, "attendance", user.uid);
+    const currentYear = new Date().getFullYear();
+    const currentMonth = new Date().getMonth() + 1;
+    const currentYearAndMonth = `${currentYear}-${currentMonth}`;
     try {
-      const documentRef = await setDoc(userDocumentRef, {
-        name : userName,
-        date : new Date(),
-        clockingIn : new Date(),
+      // コレクション参照を取得
+      const userDocumentRef = doc(
+        db,
+        "attendance",
+        user.uid,
+        currentYearAndMonth,
+        "clockingIn"
+      );
+      // ドキュメントにデータを追加
+      await setDoc(userDocumentRef, {
+        name: userName,
+        date: serverTimestamp(),
+        clockingIn: serverTimestamp(),
       });
-      console.log('Success! ドキュメントID：', documentRef);
-    } catch(e) {
+      console.log("Success! ドキュメントID：");
+    } catch (e) {
       console.log("Error", e.message);
     }
   };
@@ -89,10 +100,10 @@ export default function AttendanceButton() {
     const attendanceCollectionRef = collection(db, "attendance");
     try {
       const documentRef = await addDoc(attendanceCollectionRef, {
-        date : new Date(),
-        clockingOut : new Date(),
+        date: new Date(),
+        clockingOut: new Date(),
       });
-      console.log('Success! ドキュメントID：', documentRef.id);
+      console.log("Success! ドキュメントID：", documentRef.id);
     } catch (e) {
       console.log(e.message);
     }
@@ -101,16 +112,8 @@ export default function AttendanceButton() {
     <div>
       <div>
         <form action="">
-          <button
-            onClick={handleClockIn}
-          >
-            出勤
-          </button>
-          <button
-            onClick={handleClockOut}
-          >
-            退勤
-          </button>
+          <button onClick={handleClockIn}>出勤</button>
+          <button onClick={handleClockOut}>退勤</button>
         </form>
       </div>
     </div>
