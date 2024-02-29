9ad3a4818d199410a61dc9e1069a3d02b096a158
Ryota Nishida <24.ryota.sub@gmail.com>
2024-03-01 01:14:19 +0900

    修正

--- src/pages/AttendanceList.js
+++ src/pages/AttendanceList.js
@@ -14,30 +14,87 @@ import { db } from "../firebase";
 export default function AttendanceList() {
   const [attendanceLists, setAttendanceLists] = useState([]);
   const { user } = useAuthContext();
-  useEffect(() => {
-    // attendanceコレクションを取得
-    const attendanceCollctionRef =  db.collection("attendance").doc(user.uid);
-    const fetchAttendanceCollection = async () => {
-      try {
-        const attendanceCollctionDoc = await attendanceCollctionRef.get();
-        console.log(attendanceCollctionDoc.data());
-      } catch(e) {
-        console.error("error");
-      }
+  const convertTimestampToJapanTime = (timestamp) => {
+    if (timestamp && timestamp.toDate()) {
+      const date = timestamp.toDate();
+      const options = {
+        year: "numeric",
+        month: "numeric",
+        day: "numeric",
+        hour: "numeric",
+        minute: "numeric",
+        second: "numeric",
+        timeZone: "Asia/Tokyo", //日本時間に設定
+        weekday : 'short'
+      };
+      return new Intl.DateTimeFormat("ja-JP", options).format(date);
+    } else {
+      return "打刻なし";
     }
-    fetchAttendanceCollection();
+  };
+
+  const d = new Date();
+  const currentYear = new Date().getFullYear();
+  const currentMonth = new Date().getMonth() + 1;
+  const currentDate = new Date().getDate();
+  const dayOfWeek = d.getDay();
+  const dayNames = ["日", "月", "火", "水", "木", "金", "土"];
+  const currentYearAndMonth = `${currentYear}-${currentMonth}`;
+  const currentMonthAndDate = `${currentMonth}-${currentDate}`;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        // attendanceコレクションを取得
+        const attendanceRef = collection(db, "attendance");
+        //ユーザードキュメントを取得
+        const userDocumentRef = doc(attendanceRef, user.uid);
+        // 年月のサブコレクションを取得
+        const yearAndMonthSubCollectionRef = collection(
+          userDocumentRef,
+          currentYearAndMonth
+        );
+        // 月日のドキュメントを取得
+        const monthAndDateDocumentRef = doc(
+          yearAndMonthSubCollectionRef,
+          currentMonthAndDate
+        );
+        const docSnapShot = await getDoc(monthAndDateDocumentRef);
+        if (docSnapShot.exists()) {
+          const data = docSnapShot.data();
+          const convertedData = {
+            date: convertTimestampToJapanTime(data.date),
+            clockingIn: convertTimestampToJapanTime(data.clockingIn),
+            clockingOut: convertTimestampToJapanTime(data.clockingOut),
+            name: data.name,
+            weekday : convertTimestampToJapanTime(data.data, { weekday : 'short' }),
+          };
+          setAttendanceLists([convertedData]);
+        } else {
+          setAttendanceLists([]);
+        }
+      } catch (e) {
+        console.log(e.message);
+      }
+    };
+    fetchData();
   }, [db, user.uid]);
 
   return (
     <div>
       <h2>AttendanceList</h2>
+      <h3>{`${currentYear}年${currentMonth}月`}</h3>
+      <div>
+        <button>前月</button>
+        <button>次月</button>
+      </div>
       <div>
         {attendanceLists.map((attendance, index) => (
           <ul key={index}>
             <li>日付：{attendance.date}</li>
             <li>出勤：{attendance.clockingIn}</li>
             <li>退勤：{attendance.clockingOut}</li>
-            <li>氏名：{attendance.clockingOut}</li>
+            <li>氏名：{attendance.name}</li>
           </ul>
         ))}
       </div>
