47b593ded10cbe652cc05a11c33342aa0adb0a06
Ryota Nishida <24.ryota.sub@gmail.com>
2024-03-18 22:56:58 +0900

    修正

--- src/pages/ConfirmUserCreateInfo.js
+++ src/pages/ConfirmUserCreateInfo.js
@@ -1,12 +1,19 @@
 import React from "react";
 import Sidebar from "../components/Sidebar/Sidebar.js";
 import { useLocation } from "react-router-dom";
-import { Card, CardContent, Divider, Grid, Typography } from "@mui/material";
+import {
+  Button,
+  Card,
+  CardContent,
+  Divider,
+  Grid,
+  Typography,
+} from "@mui/material";
 import { Box } from "@mui/system";
 
-export default function ConfirmUserCreateInfo() {
-  const location = useLocation();
-  console.log(location)
+export default function ConfirmUserCreateInfo(props) {
+  const values = props.values;
+  const { hideConfirmation } = props;
   const padding = {
     padding: "18px",
   };
@@ -28,107 +35,97 @@ export default function ConfirmUserCreateInfo() {
 
   return (
     <div>
-      <Sidebar />
-      <Card sx={{ width: "72%", margin: "auto", padding: "24px" }}>
-        <CardContent>
-          <Box marginTop={"32px"} textAlign={"center"}>
-            <Typography
-              variant={variant("h6")}
-              color="text.secondary"
-              gutterBottom
+      <Box marginTop={"32px"} textAlign={"center"}>
+        <Typography variant={variant("h6")} color="text.secondary" gutterBottom>
+          以下の内容で登録しますか？
+        </Typography>
+      </Box>
+      <Grid
+        container
+        spacing={spacing}
+        sx={{ padding: padding, margin: "auto" }}
+      >
+        <Grid item xs={xs(4)}>
+          <Typography variant={variant()}>社員名：</Typography>
+        </Grid>
+        <Grid item xs={xs()}>
+          <Typography variant={variant()}>
+            {values.lastName} {values.firstName}
+          </Typography>
+        </Grid>
+        <Grid item xs={xs(4)}>
+          <Typography variant={variant()}>カナ：</Typography>
+        </Grid>
+        <Grid item xs={xs()}>
+          <Typography variant={variant()}>
+            {values.lastNameFurigana} {values.firstNameFurigana}
+          </Typography>
+        </Grid>
+
+        <Grid item xs={xs(4)}>
+          <Typography variant={variant()}>メールアドレス：</Typography>
+        </Grid>
+        <Grid item xs={xs()}>
+          <Typography variant={variant()}>{values.email}</Typography>
+        </Grid>
+
+        <Grid item xs={xs(4)}>
+          <Typography variant={variant()}>電話番号：</Typography>
+        </Grid>
+        <Grid item xs={xs()}>
+          <Typography variant={variant()}>{values.phonenumber}</Typography>
+        </Grid>
+
+        <Grid item xs={xs(4)}>
+          <Typography variant={variant()}>郵便番号：</Typography>
+        </Grid>
+        <Grid item xs={xs()}>
+          <Typography variant={variant()}>{values.postCode}</Typography>
+        </Grid>
+
+        <Grid item xs={xs(4)}>
+          <Typography variant={variant()}>都道府県：</Typography>
+        </Grid>
+        <Grid item xs={xs()}>
+          <Typography variant={variant()}>{values.prefectures}</Typography>
+        </Grid>
+
+        <Grid item xs={xs(4)}>
+          <Typography variant={variant()}>住所：</Typography>
+        </Grid>
+        <Grid item xs={xs()}>
+          <Typography variant={variant()}>
+            {values.address1}
+            {values.address2}
+          </Typography>
+        </Grid>
+
+        <Grid item xs={xs(4)}>
+          <Typography variant={variant()}>入社日：</Typography>
+        </Grid>
+        <Grid item xs={xs()}>
+          <Typography variant={variant()}>{values.joinDate}</Typography>
+        </Grid>
+
+        <Grid item xs={xs(4)}>
+          <Typography variant={variant()}>雇用形態：</Typography>
+        </Grid>
+        <Grid item xs={xs()}>
+          <Typography variant={variant()}>{values.employmentType}</Typography>
+        </Grid>
+        <Grid container spacing={spacing} sx={{ padding: padding }}>
+          <Grid item xs={2}>
+            <Button
+              type="submit"
+              onClick={hideConfirmation}
+              fullWidth
+              variant="contained"
             >
-              以下の内容で登録しますか？
-            </Typography>
-          </Box>
-          <Grid
-            container
-            spacing={spacing}
-            sx={{ padding: padding, width: "80%", margin: "auto" }}
-          >
-            <Grid item xs={xs(4)}>
-              <Typography variant={variant()}>社員名：</Typography>
-            </Grid>
-            <Grid item xs={xs()}>
-              <Typography variant={variant()}>
-                {location.state.lastName} {location.state.firstName}
-              </Typography>
-            </Grid>
-            <Grid item xs={xs(4)}>
-              <Typography variant={variant()}>カナ：</Typography>
-            </Grid>
-            <Grid item xs={xs()}>
-              <Typography variant={variant()}>
-                {location.state.lastNameFurigana}{" "}
-                {location.state.firstNameFurigana}
-              </Typography>
-            </Grid>
-
-            <Grid item xs={xs(4)}>
-              <Typography variant={variant()}>メールアドレス：</Typography>
-            </Grid>
-            <Grid item xs={xs()}>
-              <Typography variant={variant()}>
-                {location.state.email}
-              </Typography>
-            </Grid>
-
-            <Grid item xs={xs(4)}>
-              <Typography variant={variant()}>電話番号：</Typography>
-            </Grid>
-            <Grid item xs={xs()}>
-              <Typography variant={variant()}>
-                {location.state.phonenumber}
-              </Typography>
-            </Grid>
-
-            <Grid item xs={xs(4)}>
-              <Typography variant={variant()}>郵便番号：</Typography>
-            </Grid>
-            <Grid item xs={xs()}>
-              <Typography variant={variant()}>
-                {location.state.postCode}
-              </Typography>
-            </Grid>
-
-            <Grid item xs={xs(4)}>
-              <Typography variant={variant()}>都道府県：</Typography>
-            </Grid>
-            <Grid item xs={xs()}>
-              <Typography variant={variant()}>
-                {location.state.prefectures}
-              </Typography>
-            </Grid>
-
-            <Grid item xs={xs(4)}>
-              <Typography variant={variant()}>住所：</Typography>
-            </Grid>
-            <Grid item xs={xs()}>
-              <Typography variant={variant()}>
-                {location.state.address1}
-                {location.state.address2}
-              </Typography>
-            </Grid>
-
-            <Grid item xs={xs(4)}>
-              <Typography variant={variant()}>入社日：</Typography>
-            </Grid>
-            <Grid item xs={xs()}>
-              <Typography variant={variant()}>
-                {location.state.joinDate}
-              </Typography>
-            </Grid>
-
-            <Grid item xs={xs(4)}>
-              <Typography variant={variant()}>雇用形態：</Typography>
-            </Grid>
-            <Grid item xs={xs()}>
-              <Typography variant={variant()}>
-                {location.state.employmentType}
-              </Typography>
-            </Grid>
+              閉じる
+            </Button>
           </Grid>
-        </CardContent>
-      </Card>
+        </Grid>
+      </Grid>
     </div>
   );
 }
