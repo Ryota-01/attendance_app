3f7979e05ae28ec3cfe64c985662e1087c25589a
Ryota Nishida <24.ryota.sub@gmail.com>
2024-04-23 00:22:19 +0900

    firebaseConfigを.envファイルへ記述変更

--- src/firebaseConfig.dev.js
+++ src/firebaseConfig.dev.js
@@ -1,13 +1,13 @@
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfigDev = {
-  apiKey: "AIzaSyDVnSdVAdrN7a1BHnnL28-kizlntf4oBNM",
-  authDomain: "attendance-develop.firebaseapp.com",
-  projectId: "attendance-develop",
-  storageBucket: "attendance-develop.appspot.com",
-  messagingSenderId: "352308899062",
-  appId: "1:352308899062:web:f03cf2649680f3070990d7",
-  measurementId: "G-708NQRTPWD"
+  apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+  authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 export default firebaseConfigDev;
