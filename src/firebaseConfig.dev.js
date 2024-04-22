ac98baa1e573c542b35461e0379bf90b8e6738b3
Ryota Nishida <24.ryota.sub@gmail.com>
2024-04-23 08:02:42 +0900

    firebaseConfigの修正

--- src/firebaseConfig.dev.js
+++ src/firebaseConfig.dev.js
@@ -1,13 +1,13 @@
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfigDev = {
-  apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
-  authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+  apiKey: process.env.REACT_APP_DEVEROPMENT_FIREBASE_APIKEY,
+  authDomain: process.env.REACT_APP_DEVEROPMENT_FIREBASE_DOMAIN,
+  projectId: process.env.REACT_APP_DEVEROPMENT_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_DEVEROPMENT_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_DEVEROPMENT_FIREBASE_SENDER_ID,
+  appId: process.env.REACT_APP_DEVEROPMENT_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_DEVEROPMENT_FIREBASE_MEASUREMENT_ID
 };
 
 export default firebaseConfigDev;
--- src/firebaseConfig.prod.js
+++ src/firebaseConfig.prod.js
@@ -1,6 +1,13 @@
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfigProd = {
+  apiKey: process.env.REACT_APP_PRODUCTION_FIREBASE_APIKEY,
+  authDomain: process.env.REACT_APP_PRODUCTION_FIREBASE_DOMAIN,
+  projectId: process.env.REACT_APP_PRODUCTION_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_PRODUCTION_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_PRODUCTION_FIREBASE_SENDER_ID,
+  appId: process.env.REACT_APP_PRODUCTION_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_PRODUCTION_FIREBASE_MEASUREMENT_ID,
 };
 
 export default firebaseConfigProd;
