f2be0a08933392d044de5f62525996c393fd62f0
Ryota Nishida <24.ryota.sub@gmail.com>
2024-04-27 22:23:53 +0900

    更新

--- src/pages/attendancelist/AttendanceListPage.jsx
+++ src/pages/attendancelist/AttendanceListPage.jsx
@@ -45,7 +45,6 @@ export default function AttendanceList() {
               <AttendanceListTable {...props} />
             </Grid>
           </Grid>
-          
         </CardComponent>
       </NewSideBar>
     </>
