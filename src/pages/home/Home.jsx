929abf14574259f5a97779f258aa6a7baa368872
Ryota Nishida <24.ryota.sub@gmail.com>
2024-04-30 23:36:30 +0900

    0430更新

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -42,6 +42,7 @@ export default function Home() {
               <LeaveRequestComponent />
             </Grid>
           </Grid>
+          
           <AttendanceDialog />
         </NewSideBar>
       </>
